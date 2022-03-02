9838729cb3a8fe173d8cd600a748c52e9e9c5a9a
ff-josuesb <97893432+ff-josuesb@users.noreply.github.com>
2022-03-02 11:10:56 -0600

    Style added
    Issue: the screenshot is not adding bg images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import ScreenshotContainer from "./components/screenshotContainer"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/components/screenshotContainer.js
+++ src/components/screenshotContainer.js
@@ -1,5 +1,10 @@
 import { React, useRef, useState, useEffect } from "react";
 import exportAsImage from "./exportAsImage"
+import styles from "./screenshotContainer.module.css"
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 export default function ScreenshotContainer() {
     /*Image logic - square*/
@@ -33,41 +38,96 @@ export default function ScreenshotContainer() {
     }
 
     return (
-        <>
-            <div className="parent">
-                <h1>Cover test tool</h1>
-                <p><b>How to use:</b> load the images you want to preview and then click on the ...</p>
-                <input type="file" accept="image/*" onChange={onImageSquareChange} />
-                <input type="file" accept="image/*" onChange={onImageLandscapeChange} />
+        /*<Container>
+                <Row className={"parent "+styles.tests}>
+                    <h1>Cover test tool</h1>
+                    <p><b>How to use:</b> load the images you want to preview and then click on the ...</p>
+                    <input type="file" accept="image/*" onChange={onImageSquareChange} />
+                    <input type="file" accept="image/*" onChange={onImageLandscapeChange} />
 
-                <div ref={exportRef}>
-                    <CardMedium></CardMedium>
-                    <CardSmall></CardSmall>
-                    <CardSmall></CardSmall>
-                    <CardSmall></CardSmall>
-                    <br></br>
-                    <CardBig></CardBig>
-                    {ImageSquareURLs.map(ImageSquareSrc => <img src={ImageSquareSrc} key={ImageSquareSrc} />)}
-                    {ImageLandscapeURLs.map(ImageSquareSrc => <img src={ImageSquareSrc} key={ImageSquareSrc} />)}
-                </div>
+                    <div ref={exportRef}>
+                        <CardMedium></CardMedium>
+                        <CardSmall></CardSmall>
+                        <CardSmall></CardSmall>
+                        <CardSmall></CardSmall>
+                        <br></br>
+                        <CardBig></CardBig>
+                        {ImageSquareURLs.map(ImageSquareSrc => <img alt="a" src={ImageSquareSrc} key={ImageSquareSrc} />)}
+                        {ImageLandscapeURLs.map(ImageSquareSrc => <img alt="b" src={ImageSquareSrc} key={ImageSquareSrc} />)}
+                    </div>
+                </Row>
+                <Row>
+                <button onClick={() => exportAsImage(exportRef.current, "CoverTest_" + Math.floor(Math.random() * 9999))}>
+                    Download screenshot
+                </button>
+                <br></br><i>Download not working?</i> Try disabling Grammarly for Chrome and use the Desktop app instead.
+                </Row>
+            </Container>*/
+        <Container>
+            <Row>
+                <Col md={4}>1</Col>
+                <Col md={4}>2</Col>
+                <Col md={4}>
+                    <button onClick={() => exportAsImage(exportRef.current, "CoverTest_" + Math.floor(Math.random() * 9999))}>
+                        Download screenshot
+                    </button>
+                </Col>
+            </Row>
+            <div className={styles.exportContainer} ref={exportRef}>
+                <Row className={styles.previewContainer + " pt-3 pb-3"}>
+                    <Row>
+                        <h2>
+                            Card preview
+                        </h2>
+                    </Row>
+                    <Row>
+                        <Col md="4">
+                              <CardMedium test={"dsfsf"}></CardMedium>
+                        </Col>
+                        <Col md={"8"}>
+                            <Row>
+                                <Col>
+                                    <CardSmall background={'url("https://via.placeholder.com/800x800")'}></CardSmall>
+                                </Col>
+                                <Col>
+                                    <CardSmall background="blue"></CardSmall>
+                                </Col>
+                                <Col>
+                                    <CardSmall></CardSmall>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+
+                <br></br>fgdfss<hr></hr>
+                fdgdfg 
+                   
+                </Row>
             </div>
-            <button onClick={() => exportAsImage(exportRef.current, "CoverTest_" + Math.floor(Math.random() * 9999))}>
-                Download screenshot
-            </button>
-        </>
+        </Container>
     )
 }
 
 
 
-function CardSmall() {
+function CardSmall(props) {
     return (
-        <>smallxscard -</>
+        <Col className={styles.card}>
+            <div className={styles.card_content+" pt-4"} style={{background: props.background}}>
+                <img className={styles.icon} src="icon.png" alt=""></img>
+                <h3>One line title</h3>
+            </div>
+        </Col>
     )
 }
-function CardMedium() {
+function CardMedium(props) {
     return (
-        <>medcard -</>
+        <Col className={styles.card}>
+            <div className={styles.card_content+" pt-4"} style={{background: props.background}}>
+                <img className={styles.icon} src="icon.png" alt=""></img>
+                <h3>One line title</h3>
+            </div>
+        </Col>
     )
 }
 function CardBig() {
