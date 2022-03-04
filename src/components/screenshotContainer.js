577a9824c1b0e18d557cd80b8d4d21d3326a9b4d
Josue S <josb2612@gmail.com>
2022-03-03 23:22:22 -0600

    card formatted

--- src/components/screenshotContainer.js
+++ src/components/screenshotContainer.js
@@ -63,7 +63,7 @@ export default function ScreenshotContainer() {
                 <br></br><i>Download not working?</i> Try disabling Grammarly for Chrome and use the Desktop app instead.
                 </Row>
             </Container>*/
-        <Container>
+        <Container fluid>
             <Row>
                 <Col md={4}>
                     <input type="file" accept="image/*" onChange={onImageSquareChange} />
@@ -78,51 +78,36 @@ export default function ScreenshotContainer() {
                 </Col>
             </Row>
             <div className={styles.exportContainer} ref={exportRef}>
-                <Row className={styles.previewContainer + " pt-3 pb-3"}>
+                <div className={styles.previewContainer + " pt-3 pb-3"}>
                     <Row>
                         <h2>
                             Card preview
                         </h2>
-                    </Row> 
-                    {ImageSquareURLs.map(ImageSquareSrc => 
-                    <Row>
-                        <Col md="4">
-                              <CardMedium background={ImageSquareSrc}></CardMedium>
-                        </Col>
-                        <Col md={"8"}>
-                            <Row>
-                                <Col>
-                                    <CardSmall background={ImageSquareSrc}></CardSmall>
-                                </Col>
-                                <Col>
-                                    <CardSmall background={ImageSquareSrc}></CardSmall>
-                                </Col>
-                                <Col>
-                                    <CardSmall background={ImageSquareSrc}></CardSmall>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>)}
-                    {ImageSquareURLs.length > 0? "":<p><i>Cards will appear here once you upload an image</i></p> }                
-                </Row>
-                -----
-
-                <Row className={styles.previewContainer + " pt-3 pb-3"}>
+                    </Row>
+                    {ImageSquareURLs.map(ImageSquareSrc =>
+                        <Row key={ImageSquareURLs} className={styles.cardsContainer}>
+                            <CardMedium background={ImageSquareSrc} title="FF Fitness - Biceps, hombros y abs con mancuernas y algo mas de texto para ver como queda"></CardMedium>
+                            <CardSmall background={ImageSquareSrc} title="One line title"></CardSmall>
+                            <CardSmall background={ImageSquareSrc} title="TFF Fitness - Biceps, hombros y abs con mancuernas"></CardSmall>
+                            {window.innerWidth < 1745 ? <></> : <CardSmall background={ImageSquareSrc} title="FF Fitness - Biceps, hombros y abs con mancuernas y algo mas de texto para ver como queda"></CardSmall>}
+                        </Row>)}
+                    {ImageSquareURLs.length > 0 ? "" : <p><i>Cards will appear here once you upload an image</i></p>}
+                </div>
+                <br></br>
+                <div className={styles.previewContainer + " pt-3 pb-3"}>
                     <Row>
                         <h2>
-                            Card preview
+                            Hero preview
                         </h2>
-                    </Row> 
-                    {ImageLandscapeURLs.map(ImageLandscapeURLs => 
-                    <Row>
-                        <Col md="12">
-                              <CardMedium background={ImageLandscapeURLs}></CardMedium>
-                        </Col>
-                    </Row>)}
-                    {ImageLandscapeURLs.length > 0? "":<p><i>Cards will appear here once you upload an image</i></p> }                
-                </Row>
+                    </Row>
+                    {ImageLandscapeURLs.map(ImageLandscapeURLs =>
+                        <Row key={ImageLandscapeURLs} className={styles.cards_container_big}>
+                            <CardBig background={ImageLandscapeURLs} title="FF Fitness - Biceps, hombros y abs con mancuernas y algo mas de texto para ver como queda"></CardBig>
+                        </Row>)}
+                    {ImageLandscapeURLs.length > 0 ? "" : <p><i>Cards will appear here once you upload an image</i></p>}
+                </div>
             </div>
-        </Container>
+        </Container >
     )
 }
 
@@ -130,35 +115,48 @@ export default function ScreenshotContainer() {
 
 function CardSmall(props) {
     let image = "https://via.placeholder.com/50x50"
-    if (props.background != undefined){
+    if (props.background !== undefined) {
         image = props.background;
     }
     return (
-        <Col className={styles.card}>
+        <div className={styles.card}>
             <img className={styles.card_background} src={image} alt=""></img>
-            <div className="card_content">
-                <h3>One line title</h3>
+            <div className={styles.card_content}>
+                <img className={styles.icon} src="../../../icon.png" alt="icon"></img>
+                <h3 className={styles.card_title}>{props.title}</h3>
             </div>
-        </Col>
+        </div>
     )
 }
 function CardMedium(props) {
     let image = "https://via.placeholder.com/50x50"
-    if (props.background != undefined){
+    if (props.background !== undefined) {
         image = props.background;
     }
     return (
-        <Col className={styles.card}>
-            <img className={styles.card_background} src={image} alt=""></img>
-            <div className="card_content">
-                <h3>One line title</h3>
+        <div className={styles.card_medium}>
+            <img className={styles.card_background_medium} src={image} alt=""></img>
+            <div className={styles.card_content}>
+                <img className={styles.icon} src="../../../icon.png" alt="icon"></img>
+                <h3 className={styles.card_title}>{props.title}</h3>
             </div>
-        </Col>
+        </div>
     )
 }
-function CardBig() {
+function CardBig(props) {
+    let image = "https://via.placeholder.com/50x50"
+    if (props.background !== undefined) {
+        image = props.background;
+    }
     return (
-        <>bigcard -</>
+        <div className={styles.card_big}>
+            <img className={styles.card_background_big} src={image} alt=""></img>
+            <div className={styles.card_content_big}>
+                <h3 className={styles.card_title_big}>{props.title}</h3>
+                <p className={styles.card_subtitle_big}><img className={styles.icon} src="../../../icon.png" alt="icon"></img> Little description</p>
+                <div className={styles.card_button}>I am a button</div>
+            </div>
+        </div>
     )
 }
 
